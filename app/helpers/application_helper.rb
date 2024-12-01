62cf446a59a0d5105d7fd511635e7d3aa012fb57
Kieran Porter <kieranintehdas@gmail.com>
2024-12-01 23:12:29 +0000

    Start styling buttons
    - Add a helper for getting markup for a link styled as a button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def button_to(button_text, path, type = :tertiary)
+    button_type_classes = {
+      primary: "bg-red-400 text-black",
+      secondary: "text-red-400 border",
+      tertiary: "text-red-400 underline"
+    }
+
+    classes = button_type_classes.fetch(type, "")
+
+    classes += " inline-block text-center w-fit min-w-28 p-1"
+
+    unless type == :tertiary
+      classes += " rounded-md"
+    end
+
+    link_to button_text, path, class: classes
+  end
 end
