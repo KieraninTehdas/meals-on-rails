5a97d47b6665e31a90c31ff4a83ddf7360dde7f1
Kieran Porter <kieranintehdas@gmail.com>
2025-02-08 23:27:18 +0000

    Even more styling of various things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def button_to(button_text, path, type = :tertiary, icon = nil)
+  def button_to(button_text, path, type = tertiary, icon: nil, method: :get, data: {})
     button_type_classes = {
       primary: "bg-red-400 text-black",
       secondary: "text-red-400 border",
@@ -13,6 +13,27 @@ module ApplicationHelper
     unless type == :tertiary
       classes += " rounded-md min-w-28"
     end
-    render "shared/link_to_button", button_text: button_text, path: path, icon: icon, classes: classes
+
+    render(
+      "shared/link_to_button",
+      button_text: button_text,
+      path:,
+      icon:,
+      classes:,
+      method:,
+      data:
+    )
+  end
+
+  def primary_button_classes
+    "bg-red-400 text-black #{button_layout_classes} #{button_shape_classes}"
+  end
+
+  def button_layout_classes
+    "inline-block text-center p-1"
+  end
+
+  def button_shape_classes
+    "rounded-md min-w-28"
   end
 end
