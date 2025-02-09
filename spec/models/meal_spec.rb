692e2a1e54da6a31bbdac68fc53c61a35115dde1
Kieran Porter <kieranintehdas@gmail.com>
2025-02-09 21:31:59 +0000

    A few little layout tweaks and a test! A test I say!

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#recipe_book_name" do
+    subject { meal.recipe_book_name }
+
+    context "when meal is not associated with a recipe book" do
+      let(:meal) { Meal.create(name: "Dinner") }
+
+      it { is_expected.to be nil }
+    end
+
+    context "when meal is associated with a recipe book" do
+      let(:recipe_book) { RecipeBook.create(name: "Nigella Ramsay") }
+      let(:meal) { Meal.create(name: "Dinner", recipe_book: recipe_book) }
+
+      it { is_expected.to eq "Nigella Ramsay" }
+    end
+  end
 end
